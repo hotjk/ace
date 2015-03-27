f41d285741b16505f7e823897cc619468a6c60bd
weixiao <zhongwx@gmail.com>
2015-03-27 15:11:42 +0800

    UI update

--- Grit.Configuration/RabbitMQ.cs
+++ Grit.Configuration/RabbitMQ.cs
@@ -12,9 +12,9 @@ namespace Grit.Configuration
         //rabbitmqctl set_permissions -p grit guest ".*" ".*" ".*"
         //rabbitmq-plugins enable rabbitmq_management
         //RabbitMQ Management Web http://localhost:15672
-        public static readonly string ACEQueueConnectionString = "host=localhost;virtualHost=grit;username=guest;password=guest;timeout=10;prefetchcount=10";
+        //public static readonly string ACEQueueConnectionString = "host=localhost;virtualHost=grit;username=guest;password=guest;timeout=10;prefetchcount=10";
         //public static readonly string ACEQueueConnectionString = "host=192.168.3.134;virtualHost=grit;username=guest;password=guest;timeout=10;prefetchcount=50";
-        //public static readonly string ACEQueueConnectionString = "host=192.168.3.73;virtualHost=grit;username=user;password=user;timeout=10;prefetchcount=50";
+        public static readonly string ACEQueueConnectionString = "host=192.168.3.73;virtualHost=grit;username=user;password=user;timeout=10;prefetchcount=50";
         //public static readonly string ACEQueueConnectionString = "host=192.168.100.11;virtualHost=grit;username=user;password=user;timeout=10;prefetchcount=50";
     }
 }
