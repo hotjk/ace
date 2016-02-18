3d8fbf14bb464681aa5b822f5d62516849661876
weixiao <zhongwx@gmail.com>
2016-02-18 16:40:12 +0800

    watcher chart

--- Watcher.Dashboard/server.js
+++ Watcher.Dashboard/server.js
@@ -1,21 +1,23 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-var url = require('url');
-var redis = require('redis');
+﻿var redis = require('redis');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-    var queryObject = url.parse(req.url, true).query;
-    var event = queryObject.event;
-    var period = queryObject.period;
-    if (event == undefined || period == undefined) {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end("Bad Request.");
-    }
-    else {
-        var client = redis.createClient(6379, 'localhost');
-        client.hgetall(event + '_' + period, function (err, object) {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(object));
-        });
-    }
-}).listen(port);
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/event/:event/:period', function (req, res) {
+    var client = redis.createClient(6379, 'localhost');
+    var event = req.params.event;
+    var period = req.params.period;
+    client.hgetall(event + '_' + period, function (err, object) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(object));
+    });
+});
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
